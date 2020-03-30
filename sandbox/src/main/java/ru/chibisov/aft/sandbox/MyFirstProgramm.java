9289db93a1838b6108d3a6a6139ca4bb9bc83c3e
Chibisov <test2.gweb@gmail.com>
2020-03-30 12:15:13 +0300

    T2 - Добавлен класс Point

--- sandbox/src/main/java/ru/chibisov/aft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/chibisov/aft/sandbox/MyFirstProgramm.java
@@ -2,6 +2,17 @@ package ru.chibisov.aft.sandbox;
 
 public class MyFirstProgramm {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        System.out.println("Hello world!");
+
+        Point p1 = new Point(1.0, 1.0);
+        Point p2 = new Point(4.0, 4.0);
+        System.out.println(p1.distance(p2));
+        System.out.println(p2.distance(p1));
+        System.out.println(distance(p1, p2));
+        System.out.println(distance(p2, p1));
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p1.getX() - p2.getX()) * (p1.getX() - p2.getX()) + (p1.getY() - p2.getY()) * (p1.getY() - p2.getY()));
     }
 }
---
+++ sandbox/src/main/java/ru/chibisov/aft/sandbox/Point.java
@@ -0,0 +1,31 @@
+package ru.chibisov.aft.sandbox;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(Double x, Double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double distance(Point p2) {
+        return Math.sqrt((this.x - p2.x) * (this.x - p2.x) + (this.y - p2.y) * (this.y - p2.y));
+    }
+}
