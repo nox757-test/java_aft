90bdfa9431fa414244cbae94ce200a91aeb8a80c
Chibisov <test2.gweb@gmail.com>
2020-03-30 17:13:51 +0300

    T4-2 рефакторинг тест на методы

--- addressbook-web-tests/src/test/java/ru/chibisov/aft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/chibisov/aft/addressbook/GroupCreationTests.java
@@ -3,8 +3,8 @@ package ru.chibisov.aft.addressbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -17,39 +17,65 @@ public class GroupCreationTests {
     private String baseUrl;
     private StringBuffer verificationErrors = new StringBuffer();
 
-    @BeforeClass(alwaysRun = true)
+    @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver", "D:\\WorkCovid\\git\\java_aft\\addressbook-web-tests\\src\\test\\resources\\driver\\chromedriver.exe");
-        driver =  new ChromeDriver();
+        driver = new ChromeDriver();
         baseUrl = "https://www.google.com/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        login("admin", "secret");
     }
 
     @Test
     public void testNewGroupCreation() throws Exception {
-        driver.get("http://localhost/addressbook/group.php");
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys("admin");
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys("secret");
-        driver.findElement(By.xpath("//input[@value='Login']")).click();
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys("group_name");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).clear();
-        driver.findElement(By.name("group_header")).sendKeys("group_header");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys("");
-        driver.findElement(By.name("submit")).click();
+        openGroupPage();
+        openCreationGroupPage();
+        fillCreationGroup("group_header", "group_name", "group_footer");
+        submitCreationGroup();
+        backToGroupPage();
+    }
+
+    private void backToGroupPage() {
         driver.findElement(By.linkText("group page")).click();
     }
 
-    @AfterClass(alwaysRun = true)
+    private void submitCreationGroup() {
+        driver.findElement(By.name("submit")).click();
+    }
+
+    private void fillCreationGroup(String header, String name, String footer) {
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).clear();
+        driver.findElement(By.name("group_name")).sendKeys(name);
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).clear();
+        driver.findElement(By.name("group_header")).sendKeys(header);
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).clear();
+        driver.findElement(By.name("group_footer")).sendKeys(footer);
+    }
+
+    private void openCreationGroupPage() {
+        driver.findElement(By.name("new")).click();
+    }
+
+    private void openGroupPage() {
+        driver.findElement(By.linkText("groups")).click();
+    }
+
+    private void login(String login, String pass) {
+        driver.get("http://localhost/addressbook/group.php");
+        driver.findElement(By.name("user")).click();
+        driver.findElement(By.name("user")).clear();
+        driver.findElement(By.name("user")).sendKeys(login);
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).clear();
+        driver.findElement(By.name("pass")).sendKeys(pass);
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    @AfterMethod(alwaysRun = true)
     public void tearDown() throws Exception {
         driver.quit();
         String verificationErrorString = verificationErrors.toString();
